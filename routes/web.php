fd854541c4a7c65917df53590d42579ccd931475
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-01-06 09:28:17 -0800

    redirect to products list

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+   redirect(route('products.index'))->send();
+});
+
 Route::resources([
     'products' => ProductController::class,
     'collections' => CollectionController::class,
